1e8d6c7bb38b6b6a47b746b50c4332455d273daa
Piatti-Pauken <59038001+Piatti-Pauken@users.noreply.github.com>
2021-07-03 23:26:38 +0900

    ui5-buttonを挿入テスト

--- my-app/src/main/Square.js
+++ my-app/src/main/Square.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "@ui5/webcomponents/dist/Button.js";
 import "../index.css";
 
 function Square(props) {
 	return (
-	<button
+	<ui5-button
 		className="square"
 		onClick={props.onClick}
 	>
 		{props.value}
-	</button>
+	</ui5-button>
 	);
 }
 export default Square;
